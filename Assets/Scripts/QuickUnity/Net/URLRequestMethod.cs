26f56c2b3453ade5bfd4f7daa7bad7ba2af0a063
cosmos53076 <cosmos53076@163.com>
2014-03-17 09:55:05 +0800

    use MonoDevelop as default script editor

---
+++ Assets/Scripts/QuickUnity/Net/URLRequestMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickUnity.Net
+{
+		public class URLRequestMethod
+		{
+				public static string GET = "GET";
+
+				public static string POST = "POST";
+		}
+}
