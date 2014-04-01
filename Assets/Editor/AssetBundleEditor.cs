3c138b16e64fd8f78383bae15b078e7c135cade9
cosmos53076 <cosmos53076@163.com>
2014-04-01 19:00:18 +0800

    add AssetManager.cs and AssetBundleEditor.cs

---
+++ Assets/Editor/AssetBundleEditor.cs
@@ -0,0 +1,22 @@
+﻿/*************************************************************************************
+     * Class Name:  AssetBundleEditor
+     * Namespace:
+     * Author：       cosmos53076@163.com
+     * Description:  Editor functions for AssetBundle objects
+    *************************************************************************************/
+
+using System.Collections;
+using UnityEditor;
+
+public class AssetBundleEditor : Editor
+{
+    [MenuItem("QuickUnity/AssetBundle/Create AssetBundles")]
+    public static void CreateAssetBundles()
+    {
+    }
+
+    [MenuItem("QuickUnity/AssetBundle/Create AssetBundle All In One")]
+    public static void CreateAssetBundleAllInOne()
+    {
+    }
+}
---
+++ Assets/Scripts/QuickUnity/Managers/AssetManager.cs
@@ -0,0 +1,24 @@
+﻿/*************************************************************************************
+     * Class Name:  AssetManager
+     * Namespace:
+     * Author：       cosmos53076@163.com
+     * Description:  A manager class to handle something about assets.
+    *************************************************************************************/
+
+using System.Collections;
+using UnityEngine;
+
+public class AssetManager : MonoBehaviour
+{
+    private static AssetManager instance;
+
+    public static AssetManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
