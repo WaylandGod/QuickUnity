9122a3a8d22f82de70e14d828ae6758100a891eb
jerry.li <cosmos53076@163.com>
2014-04-26 19:02:00 +0800

    initialize FSM folder.

---
+++ Assets/Scripts/QuickUnity/FSM/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace QuickUnity.FSM
+{
+    /// <summary>
+    /// A entity to handle state.
+    /// </summary>
+    public abstract class Entity : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Scripts/QuickUnity/FSM/StateMachine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace QuickUnity.FSM
+{
+    /// <summary>
+    /// State controller.
+    /// </summary>
+    public class StateMachine : MonoBehaviour
+    {
+        private State currentState = null;
+
+        /// <summary>
+        /// The current state.
+        /// </summary>
+        public State CurrentState
+        {
+            get
+            {
+                return currentState;
+            }
+        }
+    }
+}
