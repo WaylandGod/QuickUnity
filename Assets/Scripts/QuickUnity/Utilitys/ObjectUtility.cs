9efab258e6fb51fbbe85c465f4e71c33d109d6a2
cosmos53076 <cosmos53076@163.com>
2014-07-30 19:18:26 +0800

    add ObjectUtility.cs

---
+++ Assets/Scripts/QuickUnity/Utilitys/ObjectUtility.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// The Utilitys namespace.
+/// </summary>
+namespace QuickUnity.Utilitys
+{
+    /// <summary>
+    /// A class to process object things. This class cannot be inherited.
+    /// </summary>
+    public sealed class ObjectUtility
+    {
+        /// <summary>
+        /// Deeps clone.
+        /// </summary>
+        /// <param name="source">The object of source.</param>
+        /// <returns>System.Object.</returns>
+        public object DeepClone(object source)
+        {
+            BinaryFormatter fomatter = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.Clone));
+            MemoryStream stream = new MemoryStream();
+            fomatter.Serialize(stream, source);
+            stream.Position = 0;
+            object clone = fomatter.Deserialize(stream);
+            stream.Close();
+            return clone;
+        }
+    }
+}
